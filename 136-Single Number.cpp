f192888b7beb70f90aa0bd1c2b8c705805ca8c63
kycd <kycd.dark@gmail.com>
2016-05-02 11:29:10 +0800

    136 AC Code

---
+++ 136-Single Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int Ans = 0;
+        for(int i = 0; i < nums.size(); ++i)
+            Ans ^= nums[i];
+        return Ans;
+    }
+};
