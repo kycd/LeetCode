8022d4ef3486c1847466549de28269c130433807
Kevin Cheng <kevincheng@kkbox.com>
2017-04-18 18:26:59 +0800

    374 fix tab

--- 374-Guess Number Higher or Lower.cpp
+++ 374-Guess Number Higher or Lower.cpp
@@ -4,26 +4,26 @@
 int guess(int num);
 
 class Solution {
-		private:
-				int guessNumber(int min, int max) {
-						int mid = ((unsigned int) min + max) / 2;
-						int guessResult = guess(mid);
+	private:
+		int guessNumber(int min, int max) {
+			int mid = ((unsigned int) min + max) / 2;
+			int guessResult = guess(mid);
 
-						// cout << "( " << min << ", " << mid << " ," << max << ") = " << guessResult << endl;
-						switch(guessResult) {
-								case -1:
-										return guessNumber(min, mid - 1);
-										break;
-								case 1:
-										return guessNumber(mid + 1, max);
-										break;
-								case 0:
-										return mid;
-										break;
-						}
-				}
-		public:
-				int guessNumber(int n) {
-						return guessNumber(1, n);
-				}
+			// cout << "( " << min << ", " << mid << " ," << max << ") = " << guessResult << endl;
+			switch(guessResult) {
+				case -1:
+					return guessNumber(min, mid - 1);
+					break;
+				case 1:
+					return guessNumber(mid + 1, max);
+					break;
+				case 0:
+					return mid;
+					break;
+			}
+		}
+	public:
+		int guessNumber(int n) {
+			return guessNumber(1, n);
+		}
 };
